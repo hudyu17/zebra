f575b4f8e63896dd164f549eb3942a76e6ebbfd8
hudyu17 <hengjeung.yuen@gmail.com>
2023-01-14 10:55:44 -0600

    sidebar edits

--- src/components/crosswalkPanel.js
+++ src/components/crosswalkPanel.js
@@ -127,108 +127,34 @@ export default function CrosswalkPanel({ open, setOpen, marker, session }) {
 
                             
 
-                            <div className="pt-8">
+                            <div className="pt-8 space-y-6">
                               <div>
                                 <h3 className="text-lg font-medium leading-6 text-gray-900">Other</h3>
                                 <p className="mt-1 text-sm text-gray-500">
                                   Some wrap-up details from us.
                                 </p>
                               </div>
-                              <div className="mt-6">
-                                <fieldset>
-                                  <legend className="sr-only">By Email</legend>
-                                  <div className="text-base font-medium text-gray-900" aria-hidden="true">
-                                    Information to share alongside your crosswalk suggestion
-                                  </div>
-                                  <div className="mt-4 space-y-4">
-                                    <div className="relative flex items-start">
-                                      <div className="flex h-5 items-center">
-                                        <input
-                                          id="comments"
-                                          name="comments"
-                                          type="checkbox"
-                                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                      </div>
-                                      <div className="ml-3 text-sm">
-                                        <label htmlFor="comments" className="text-gray-500">
-                                          <span className='font-medium text-gray-700'>Name only:</span> {session.user.name}
-                                        </label>
-                                      </div>
-                                    </div>
-                                    <div className="relative flex items-start">
-                                      <div className="flex h-5 items-center">
-                                        <input
-                                          id="candidates"
-                                          name="candidates"
-                                          type="checkbox"
-                                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                      </div>
-                                      <div className="ml-3 text-sm">
-                                        <label htmlFor="candidates" className="font-medium text-gray-700">
-                                          Candidates
-                                        </label>
-                                        <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
-                                      </div>
-                                    </div>
-                                    <div className="relative flex items-start">
-                                      <div className="flex h-5 items-center">
-                                        <input
-                                          id="offers"
-                                          name="offers"
-                                          type="checkbox"
-                                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                        />
-                                      </div>
-                                      <div className="ml-3 text-sm">
-                                        <label htmlFor="offers" className="font-medium text-gray-700">
-                                          Offers
-                                        </label>
-                                        <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </fieldset>
-                                <fieldset className="mt-6">
-                                  <legend className="contents text-base font-medium text-gray-900">Push Notifications</legend>
-                                  <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
-                                  <div className="mt-4 space-y-4">
-                                    <div className="flex items-center">
-                                      <input
-                                        id="push-everything"
-                                        name="push-notifications"
-                                        type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                      />
-                                      <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
-                                        Everything
-                                      </label>
-                                    </div>
-                                    <div className="flex items-center">
-                                      <input
-                                        id="push-email"
-                                        name="push-notifications"
-                                        type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                      />
-                                      <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
-                                        Same as email
-                                      </label>
-                                    </div>
-                                    <div className="flex items-center">
-                                      <input
-                                        id="push-nothing"
-                                        name="push-notifications"
-                                        type="radio"
-                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                      />
-                                      <label htmlFor="push-nothing" className="ml-3 block text-sm font-medium text-gray-700">
-                                        No push notifications
-                                      </label>
-                                    </div>
-                                  </div>
-                                </fieldset>
+                              <div>
+                                <div>
+                                  <label htmlFor="location" className="block text-sm font-medium text-gray-700">
+                                    What information would you like to share pubicly alongside your suggested crosswalk?
+                                  </label>
+                                  <select
+                                    id="location"
+                                    name="location"
+                                    className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    // defaultValue=""
+                                  >
+                                    <option>Name ({session.user.name}) and Profile Image</option>
+                                    <option>Name only ({session.user.name})</option>
+                                    <option>Anonymous</option>
+                                  </select>
+                                </div>
+                               
+                              </div>
+                              <div>
+                                <p className="block text-sm font-medium text-gray-700">Your suggestion will be live in a moment.</p>
+                                <p className="block text-sm text-gray-500">In case of any issues, we'll contact you at: <span className='text-gray-700 font-medium'>{session.user.email}</span></p>
                               </div>
                             </div>
                           </div>
@@ -245,7 +171,7 @@ export default function CrosswalkPanel({ open, setOpen, marker, session }) {
                                 type="submit"
                                 className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                               >
-                                Save
+                                Submit
                               </button>
                             </div>
                           </div>
